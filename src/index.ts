c013d39c690399d1f3a0e1836b00952e7f42f0d5
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-22 22:06:50 +0900

    Elysia app 분기 테스트

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000, () => {
-  console.log("[Win Yourself]:: Server Start 3000 port")
+const app = new Elysia();
+
+app.get("/", () => "Hello Elysia");
+app.listen(3000, () => {
+  console.log("[Win Yourself]:: Server Start 3000 port");
 });
