767e4e434fac5998978279cfc8580d4a702c9e55
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-24 19:23:17 +0900

    elysia + prisma 연동

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { PrismaClient } from "@prisma/client";
 import { Elysia } from "elysia";
 
 const app = new Elysia();
 
+const prisma = new PrismaClient();
+
 app.get("/", () => "Hello Elysia");
+app.get("/users", async () => {
+  const users = await prisma.user.findMany();
+  return users;
+});
 app.listen(3000, () => {
   console.log("[Win Yourself]:: Server Start 3000 port");
 });
