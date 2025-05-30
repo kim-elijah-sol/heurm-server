a2c27f5523afeb88dfef1d47b3fc4db6f2ccf10c
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-30 13:31:12 +0000

    refactoring: [POST] /user/join/verify-email -> createAPI 로 처리

--- src/domain/user/join/verify-email.post.ts
+++ src/domain/user/join/verify-email.post.ts
@@ -1,37 +1,37 @@
 import { t } from 'elysia';
-import { AppContext, redisClient } from '~/app';
+import { redisClient } from '~/app';
 import { EMAIL_VERIFY_EXPIRE, EMAIL_VERIFY_OK } from '~/lib/constant';
+import { createAPI } from '~/lib/create-api';
 import { BadRequestError } from '~/lib/error';
 import { RedisKeyStore } from '~/lib/redis-key-store';
 import { v } from '~/lib/validator';
 
-export const verifyEmail = async ({
-  query: { code, id, email },
-}: AppContext) => {
-  const redisKey = RedisKeyStore.verifyEmail(id, email);
+export const verifyEmail = createAPI(
+  async ({ query: { code, id, email } }) => {
+    const redisKey = RedisKeyStore.verifyEmail(id, email);
 
-  const codeInRedis = await redisClient.get(redisKey);
+    const codeInRedis = await redisClient.get(redisKey);
 
-  if (codeInRedis === null) {
-    throw new BadRequestError('not exist verify infomation');
+    if (codeInRedis === null) {
+      throw new BadRequestError('not exist verify infomation');
+    }
+
+    const result = code === codeInRedis;
+
+    if (result) {
+      redisClient.set(redisKey, EMAIL_VERIFY_OK);
+      redisClient.expire(redisKey, EMAIL_VERIFY_EXPIRE);
+    }
+
+    return {
+      result,
+    };
+  },
+  {
+    query: t.Object({
+      code: v.isVerifyCode,
+      id: t.String(),
+      email: v.isEmail,
+    }),
   }
-
-  const result = code === codeInRedis;
-
-  if (result) {
-    redisClient.set(redisKey, EMAIL_VERIFY_OK);
-    redisClient.expire(redisKey, EMAIL_VERIFY_EXPIRE);
-  }
-
-  return {
-    result,
-  };
-};
-
-verifyEmail.model = {
-  query: t.Object({
-    code: v.isVerifyCode,
-    id: t.String(),
-    email: v.isEmail,
-  }),
-};
+);
