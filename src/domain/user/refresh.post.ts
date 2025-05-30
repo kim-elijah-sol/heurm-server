5743d48385563de1256d2539df22ce6847e002cb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-30 13:36:20 +0000

    refactoring: [POST] /user/refresh -> createAPI 로 처리

--- src/domain/user/refresh.post.ts
+++ src/domain/user/refresh.post.ts
@@ -1,45 +1,39 @@
 import { t } from 'elysia';
-import { AppContext, redisClient } from '~/app';
+import { redisClient } from '~/app';
+import { createAPI } from '~/lib/create-api';
 import { UnauthorizedError } from '~/lib/error';
 import { RedisKeyStore } from '~/lib/redis-key-store';
 
-export const refresh = async ({
-  body: { refreshToken, clientId },
-  rtJWT,
-  atJWT,
-}: AppContext<{
-  body: typeof refreshBodyModel.static;
-}>) => {
-  const clientIdInRedis = await redisClient.get(
-    RedisKeyStore.refreshToken(refreshToken)
-  );
+export const refresh = createAPI(
+  async ({ body: { refreshToken, clientId }, rtJWT, atJWT }) => {
+    const clientIdInRedis = await redisClient.get(
+      RedisKeyStore.refreshToken(refreshToken)
+    );
 
-  if (clientIdInRedis !== clientId) {
-    throw new UnauthorizedError('authorization error');
+    if (clientIdInRedis !== clientId) {
+      throw new UnauthorizedError('authorization error');
+    }
+
+    const verifyResult = await rtJWT.verify(refreshToken);
+
+    if (verifyResult === false) {
+      throw new UnauthorizedError('authorization error');
+    }
+
+    const { id } = verifyResult as { id: string };
+
+    const accessToken = await atJWT.sign({
+      id,
+    });
+
+    return {
+      accessToken,
+    };
+  },
+  {
+    body: t.Object({
+      refreshToken: t.String(),
+      clientId: t.String(),
+    }),
   }
-
-  const verifyResult = await rtJWT.verify(refreshToken);
-
-  if (verifyResult === false) {
-    throw new UnauthorizedError('authorization error');
-  }
-
-  const { id } = verifyResult as { id: string };
-
-  const accessToken = await atJWT.sign({
-    id,
-  });
-
-  return {
-    accessToken,
-  };
-};
-
-const refreshBodyModel = t.Object({
-  refreshToken: t.String(),
-  clientId: t.String(),
-});
-
-refresh.model = {
-  body: refreshBodyModel,
-};
+);
