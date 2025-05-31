e13000621b93a55147f82e91f5f7bf8071af5cfb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-05-31 14:09:41 +0000

    chore: swagger 로 openAPI document 제공

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import cors from '@elysiajs/cors';
 import jwt from '@elysiajs/jwt';
+import swagger from '@elysiajs/swagger';
 import Elysia, { InferContext, RouteSchema, status } from 'elysia';
 import * as e from '~/lib/error';
 import { prismaClient } from './prisma-client';
@@ -19,6 +20,7 @@ export const app = new Elysia()
     })
   )
   .use(cors())
+  .use(swagger())
   .decorate('prismaClient', prismaClient)
   .decorate('redisClient', redisClient)
   .onError(({ error }) => {
