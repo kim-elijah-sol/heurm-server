bde24f3788319efaef34c8d981a2c031c8eca644
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-02 14:30:05 +0000

    type: createAPI 타입 추론 강화

--- src/lib/create-api/create-api.ts
+++ src/lib/create-api/create-api.ts
@@ -1,11 +1,27 @@
-import { RouteSchema } from 'elysia';
+import { RouteSchema, t } from 'elysia';
 import { AppContext } from '~/app';
 
+type ParseSchemaEntity<
+  Key extends keyof RouteSchema,
+  Schema extends RouteSchema = {}
+> = Schema[Key] extends ReturnType<(typeof t)['Object']>
+  ? Schema[Key]['static']
+  : unknown;
+
+type ParseSchema<Schema extends RouteSchema = {}> = {
+  body: ParseSchemaEntity<'body', Schema>;
+  headers: ParseSchemaEntity<'headers', Schema>;
+  query: ParseSchemaEntity<'query', Schema>;
+  params: ParseSchemaEntity<'params', Schema>;
+  cookie: ParseSchemaEntity<'cookie', Schema>;
+  response: ParseSchemaEntity<'response', Schema>;
+};
+
 export const createAPI = <T, Schema extends RouteSchema = {}>(
-  apiHandler: (context: AppContext<Schema>) => Promise<T>,
+  apiHandler: (context: AppContext<ParseSchema<Schema>>) => Promise<T>,
   model: Schema
 ): {
-  (context: AppContext<Schema>): Promise<T>;
+  (context: AppContext<ParseSchema<Schema>>): Promise<T>;
   model: Schema;
 } => {
   return Object.assign(apiHandler, {
