a12c0d021039f1603854d6cf186a6e533c21728d
kim-elijah-sol <kimelijahsol@gmail.com>
2025-06-10 13:53:40 +0000

    feature: update `targetCount` when update `challengeItem`

--- src/domain/challenge/challenge-item/challenge-item.patch.ts
+++ src/domain/challenge/challenge-item/challenge-item.patch.ts
@@ -1,14 +1,27 @@
+import { fromZonedTime } from 'date-fns-tz';
 import { t } from 'elysia';
+import { getUserTimezone } from '~/lib/cache';
 import { createAPI } from '~/lib/create-api';
 import { BadRequestError } from '~/lib/error';
 import { v } from '~/lib/validator';
 
 export const patchChallengeItem = createAPI(
   async ({
-    body: { challengeId, challengeItemId, name, type, days, unit, targetCount },
+    body: {
+      challengeId,
+      challengeItemId,
+      name,
+      type,
+      days,
+      date: _date,
+      unit,
+      targetCount,
+    },
     userId,
     prismaClient,
   }) => {
+    const userTimezone = await getUserTimezone(userId!);
+
     const challenge = await prismaClient.challenge.findUnique({
       where: {
         id: challengeId,
@@ -21,6 +34,24 @@ export const patchChallengeItem = createAPI(
 
     if (challenge == null) {
       throw new BadRequestError('can not find challenge data');
+    }
+
+    if (
+      type !== 'COMPLETE' &&
+      targetCount !== null &&
+      targetCount !== undefined
+    ) {
+      const date = fromZonedTime(new Date(`${_date} 00:00:00`), userTimezone);
+
+      await prismaClient.challengeItemHistory.updateMany({
+        where: {
+          challengeItemId,
+          date,
+        },
+        data: {
+          targetCount,
+        },
+      });
     }
 
     await prismaClient.challengeItem.update({
@@ -48,6 +79,7 @@ export const patchChallengeItem = createAPI(
       name: v.isChallengeItemName,
       type: v.isChallengeItemType,
       days: v.isChallengeItemDays,
+      date: v.isDate,
       unit: t.Optional(t.Nullable(t.String())),
       targetCount: t.Optional(t.Nullable(t.Number())),
     }),
