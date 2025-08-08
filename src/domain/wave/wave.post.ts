d9ef3a998ed2447918ded5c7cdb569117c9104eb
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-08 13:29:40 +0000

    feature: [POST] /wave 이름 중복 처리

--- src/domain/wave/wave.post.ts
+++ src/domain/wave/wave.post.ts
@@ -1,8 +1,20 @@
 import { t } from 'elysia';
 import { createAPI } from '~/lib/create-api';
+import { ConflictError } from '~/lib/error';
 
 export const postWave = createAPI(
   async ({ body: { name }, prismaClient, userId }) => {
+    const conflictWaveForName = await prismaClient.wave.findFirst({
+      where: {
+        userId,
+        name,
+      },
+    });
+
+    if (conflictWaveForName !== null) {
+      throw new ConflictError(`${name} flow is already added.`);
+    }
+
     const result = await prismaClient.wave.create({
       data: {
         userId: userId!,
