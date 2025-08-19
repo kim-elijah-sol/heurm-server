5630a5b2e60694bf33a0b1a705fc9f6eb095a81e
kim-elijah-sol <kimelijahsol@gmail.com>
2025-08-19 13:15:00 +0000

    remove: refresh token 관련 redis key 제거

--- src/lib/redis-key-store/index.ts
+++ src/lib/redis-key-store/index.ts
@@ -1,6 +1,5 @@
 export const RedisKeyStore = {
   verifyEmail: (id: string, email: string) => `vefify-email:${id}:${email}`,
-  refreshToken: (refreshToken: string) => `refresh-token:${refreshToken}`,
   resetPasswordVerifyEmail: (id: string, email: string) =>
     `reset-password-vefify-email:${id}:${email}`,
   userTimezone: (userId: string) => `user-time-zone:${userId}`,
